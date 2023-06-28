47a703caeeec65db8798d290560f7957b5c45c29
AlexBoynich <101637186+AlexBoynich@users.noreply.github.com>
2023-06-28 14:12:21 +0700

    yandexMetrica

--- src/main.js
+++ src/main.js
@@ -2,16 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
-import VueYandexMetrika from 'vue-yandex-metrika' 
+import vuetify from './plugins/vuetify' 
 
 Vue.config.productionTip = false
 
-Vue.use(VueYandexMetrika, {
-  id: 93952563,
-  env: process.env.NODE_ENV,
-  router: router,
-})
 
 new Vue({
   router,
